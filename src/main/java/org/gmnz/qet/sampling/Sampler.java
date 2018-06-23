36d1bae93831c287f4de53bd9c9aefd77998cc29
S3lvatico <s3lvatico@gmail.com>
2018-06-23 15:11:17 +0200

    Sampler : il sampleHolder non è necessariamente controllato da qui

--- src/main/java/org/gmnz/qet/sampling/Sampler.java
+++ src/main/java/org/gmnz/qet/sampling/Sampler.java
@@ -2,16 +2,38 @@ package org.gmnz.qet.sampling;
 
 
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+
 public class Sampler {
 
-	private SampleHolderImpl sampleHolder;
+// private SampleHolder sampleHolder;
 
 	private CommonLogFileSamplerTask samplerTask;
 
+	private ExecutorService exec;
 
 
-	public Sampler(String targetFileName) {
-		sampleHolder = new SampleHolderImpl();
+
+	public Sampler(String targetFileName, SampleHolder sampleHolder) throws FileSamplerException {
+// this.sampleHolder = sampleHolder;
+		samplerTask = new CommonLogFileSamplerTask(targetFileName, sampleHolder);
+		exec = Executors.newSingleThreadExecutor();
+	}
+
+
+
+	public void start() {
+		exec.execute(samplerTask);
+		exec.shutdown();
+	}
+
+
+
+	public void stop() {
+		samplerTask.setSamplingActive(false);
 	}
 
 }
