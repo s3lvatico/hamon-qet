9e03b387658151a32b072f56f6b078e5c8c80c25
S3lvatico <s3lvatico@gmail.com>
2018-06-26 17:50:26 +0200

    primo abbozzo del test per il sampler

--- src/test/java/org/gmnz/qet/sampling/SamplerTest.java
+++ src/test/java/org/gmnz/qet/sampling/SamplerTest.java
@@ -3,4 +3,22 @@ package org.gmnz.qet.sampling;
 
 public class SamplerTest {
 
+	private Sampler sampler;
+	private SampleHolder sampleHolder;
+
+
+
+
+	public SamplerTest() throws FileSamplerException {
+		sampleHolder = new SampleHolderImpl();
+		sampler = new Sampler("example.log", sampleHolder);
+	}
+
+
+
+
+	public static void main(String[] args) throws FileSamplerException {
+		SamplerTest test = new SamplerTest();
+
+	}
 }
